a396e98f051a35a1d08388702c54aa9da7c4d50d
Tim <tsbkwd@mst.edu>
2018-01-17 12:48:35 -0600

    Added properities that were missed in the PeopleDTO and added the new properities to People.cs

--- TestWebApi/Domain/Entities/People.cs
+++ TestWebApi/Domain/Entities/People.cs
@@ -7,10 +7,24 @@ namespace TestWebApi.Domain.Entities
         public string LastName { get; set; }
         public string Address1{ get; set; }
         public string Address2 { get; set; }
-        public int ZipCode { get; set; }
+        public string City { get; set; }
         public string State { get; set; }
-        public string PhoneNumber { get; set; }
+        public int Zipcode { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Skill1 { get; set; }
+        public string Skill2 { get; set; }
+        public string Skill3 { get; set; }
+        public string Company { get; set; }
         public string Notes { get; set; }
+        public string Type { get; set; }
+        public string POC { get; set; }
+        public int YearsExp { get; set; }
+
+        public float PayRate { get; set; }
+
+        public float BillRate { get; set; }
+        
+        public System.DateTime AvailableDate { get; set; }        
     }
 }
--- TestWebApi/Services/Dtos/PeopleDto.cs
+++ TestWebApi/Services/Dtos/PeopleDto.cs
@@ -8,6 +8,7 @@ namespace TestWebApi.Services.Dtos
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address1 { get; set; }
+        public string Address2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public int Zipcode { get; set; }
